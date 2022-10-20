e3d217e70a8b1054f25fb4bf8f22432e9d260c32
MoisesSV <2120200388@soy.utj.edu.mx>
2022-10-19 21:47:41 -0500

    clase del dia 19 de octubre

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -18,12 +18,81 @@ class VideoController extends Controller
      */
     public function index()
     {
-        //mostar el formulario de captura
-        $videos = VsVideo :: where ('activo', '=', 1) -> get() ;
-        return view(view:'videos.index')-> with('videos', $videos);
+        //Carga la página de inicio del objeto
+        $vs_videos = VsVideo :: where ('active', '=', 1) -> get() ;
+        $cont = VsVideo::count();
+        $videos = $this->cargarDT($vs_videos);
+        return view(view:'videos.index')-> with('videos', $videos)->with('cont', $cont);
+        
     }
 
-    /**
+public function cargarDT($consulta)
+    {
+        $videos = [];
+
+        foreach ($consulta as $key => $value){
+
+            $ruta = "eliminar".$value['id'];
+            //$eliminar = route('delete-video', $value['id']);
+            $eliminar='#';
+            $actualizar =  route('videos.edit', $value['id']);
+
+            $acciones = '
+                <div class="btn-acciones">
+                    <div class="btn-circle">
+                        <a href="'.$actualizar.'" role="button" class="btn btn-success" title="Actualizar">
+                            <i class="far fa-edit"></i>
+                        </a>
+                         <a href="#'.$ruta.'" role="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#'.$ruta.'">
+                            <i class="far fa-trash-alt"></i>
+                        </a>
+
+                    </div>
+                </div>
+<!-- Modal -->
+            <div class="modal fade" id="'.$ruta.'" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">¿Seguro que deseas eliminar este video?</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="text-primary">
+                        <small>
+                            '.$value['id'].', '.$value['title'].'                 </small>
+                      </p>
+</div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                      <a href="'.$eliminar.'" type="button" class="btn btn-danger">Eliminar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            ';
+$videos[$key] = array(
+                $acciones,
+                $value['id'],
+                $value['title'],
+                $value['description'],
+                $value['image'],
+                $value['video_path'],
+                $value['name'],
+                $value['email']
+
+            );
+
+        }
+
+        return $videos;
+    }
+
+
+    /**m run dev
+
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
@@ -139,7 +208,7 @@ class VideoController extends Controller
         }
 
         $video->update();
-        return redirect()->route(route: 'home')->with(array(
+        return redirect()->route(route: 'videos.index')->with(array(
             'message' => 'El video se ha subido correctamente'
         ));
     }
